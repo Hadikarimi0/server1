38a0f2dadc89f743332ec65fbb20012080768339
Hadikarimi0 <114799735+Hadikarimi0@users.noreply.github.com>
2024-01-18 11:03:41 +0330

    Update tiker.py

--- tiker.py
+++ tiker.py
@@ -1,21 +1,2 @@
-import os
-os.system("pip install rubpy==6.4.6")
-from rubpy.sync import Client
-from time import sleep
-
-
-#guid = "c0Balnh03d6f6fba81235d6eba42f0e5"
-#guid2 = #"c0yQCt0fbc97596f766c6ba037c652b2"
-#tim = 2
-#user = "jsjshsbshshsjsjs"
-#user2 = "pahlavan"
-
-with Client(session='bot') as client:
-	while True:
-	
-	   h1 = client.update_channel_username(channel_guid=guid, username=user)
-	   h2 = client.update_channel_username(channel_guid=guid2, username=user2)
-
-	   sleep(tim)
-	   print(h1,h2)
-	   
+print("")
+print("نصف یوزرارو زدی داداش. ۲۰ نفر اومدن پیویم که تیکرو ازت غیر فعال کنم واقعا شرمندم", end="")
